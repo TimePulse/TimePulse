5ab6b27b0b7481a20d86c84a42570edee194e6cd
Evan Dorn <evan@lrdesign.com>
2011-05-20 19:04:35 -0700

    ran view spec upgrade script

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,41 @@
+# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def admin?
+    logged_in? and current_user.admin?
+  end
+
+  def current_project_label
+    returning String.new do |str|
+      if current_project
+        str << current_project.name
+        str << "&nbsp;&nbsp;[#{current_project.client.name }]"  if current_project.client
+      else
+        str << "None Selected"
+      end
+      str.html_safe!
+    end
+  end
+
+  def clocked_in_project?(project)
+    return true if current_work_unit && current_work_unit.project == project
+  end
+
+  def labeled_datepicker_field(form, field_name, options = {})
+    options.reverse_merge!(:cssclass => :date_entry)
+    fieldval =  form.object.send(field_name)
+    value = fieldval ? fieldval.to_s(:long) : nil
+    labeled_input form, field_name, options, {
+      :class => options[:cssclass],
+      :value => value
+    }
+  end
+
+  def labeled_datetimepicker_field(form, field_name, options = {})
+    labeled_datepicker_field(form, field_name, options.merge!( :cssclass => :datetime_entry ))
+  end
 end
