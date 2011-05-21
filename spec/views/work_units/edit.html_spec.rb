fe39c668f4da8ca2c996ea86ff81dc2b8117983e
Evan Dorn <evan@lrdesign.com>
2011-05-21 16:35:33 -0700

    fixed abother view spec

--- spec/views/work_units/edit.html_spec.rb
+++ spec/views/work_units/edit.html_spec.rb
@@ -2,20 +2,18 @@ require 'spec_helper'
 
 describe "/work_units/edit" do
   include WorkUnitsHelper
-  
+
   before(:each) do
     assign(:work_unit, @work_unit = Factory(:work_unit))
   end
-  
+
   it "should succeed" do
     render
-    
   end
 
   it "should render edit form" do
     render
-    
-    rendered.should have_selector("form[action=#{work_unit_path(@work_unit)}][method='post']") do |scope|
+    rendered.should have_selector("form[action='#{work_unit_path(@work_unit)}'][method='post']") do |scope|
       scope.should have_selector("input#work_unit_hours[name='work_unit[hours]']")
       scope.should have_selector("input#work_unit_notes[name='work_unit[notes]']")
     end
