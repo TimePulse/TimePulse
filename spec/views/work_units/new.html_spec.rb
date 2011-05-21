137f5507024346760f075f3cd989b862aa0bc0af
Evan Dorn <evan@lrdesign.com>
2011-05-21 16:36:31 -0700

    fixed another view spec

--- spec/views/work_units/new.html_spec.rb
+++ spec/views/work_units/new.html_spec.rb
@@ -2,21 +2,19 @@ require 'spec_helper'
 
 describe "/work_units/new" do
   include WorkUnitsHelper
-  
+
   before(:each) do
     assign(:work_unit, Factory.build(:work_unit))
   end
-  
+
   it "should succeed" do
     render
-    
   end
-  
+
 
   it "should render new form" do
     render
-    
-    rendered.should have_selector("form[action=?][method='post']", work_units_path) do |scope|
+    rendered.should have_selector("form[action='#{work_units_path}'][method='post']") do |scope|
       scope.should have_selector("input#work_unit_hours[name='work_unit[hours]']")
       scope.should have_selector("input#work_unit_notes[name='work_unit[notes]']")
     end
