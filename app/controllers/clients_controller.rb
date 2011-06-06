9fb3745c307a96699fbe952b1f6f4d369087c12c
Evan Dorn <evan@lrdesign.com>
2011-06-06 12:33:02 -0700

    converted clients controller specs from mock/stub style to factories.  Also removed XML cruft from clients controller.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,35 +1,18 @@
 class ClientsController < ApplicationController
+
   # GET /clients
-  # GET /clients.xml
   def index
     @clients = Client.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @clients }
-    end
   end
 
   # GET /clients/1
-  # GET /clients/1.xml
   def show
     @client = Client.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @client }
-    end
   end
 
   # GET /clients/new
-  # GET /clients/new.xml
   def new
     @client = Client.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @client }
-    end
   end
 
   # GET /clients/1/edit
@@ -38,19 +21,14 @@ class ClientsController < ApplicationController
   end
 
   # POST /clients
-  # POST /clients.xml
   def create
     @client = Client.new(params[:client])
 
-    respond_to do |format|
-      if @client.save
-        flash[:notice] = 'Client was successfully created.'
-        format.html { redirect_to(@client) }
-        format.xml  { render :xml => @client, :status => :created, :location => @client }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @client.errors, :status => :unprocessable_entity }
-      end
+    if @client.save
+      flash[:notice] = 'Client was successfully created.'
+      redirect_to(@client)
+    else
+      render :action => "new"
     end
   end
 
@@ -59,27 +37,19 @@ class ClientsController < ApplicationController
   def update
     @client = Client.find(params[:id])
 
-    respond_to do |format|
-      if @client.update_attributes(params[:client])
-        flash[:notice] = 'Client was successfully updated.'
-        format.html { redirect_to(@client) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @client.errors, :status => :unprocessable_entity }
-      end
+    if @client.update_attributes(params[:client])
+      flash[:notice] = 'Client was successfully updated.'
+      redirect_to(@client)
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /clients/1
-  # DELETE /clients/1.xml
   def destroy
     @client = Client.find(params[:id])
     @client.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(clients_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(clients_url)
   end
 end
