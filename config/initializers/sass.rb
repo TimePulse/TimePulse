1fb0760cac5a1535ab0a1279718d4c7fc937421e
Evan Dorn <evan@lrdesign.com>
2011-06-09 20:23:34 -0700

    upgraded sassfiles to 3.0 style

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,3 +1,1 @@
-Sass::Plugin.options[:template_location] = {
-  "#{Rails.root}/app/stylesheets" => "#{Rails.root}/public/stylesheets"
-}
+Sass::Plugin.options[:template_location] = "#{Rails.root}/app/stylesheets"
