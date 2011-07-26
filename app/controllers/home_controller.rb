4a6b49a07852e9eb2db247235b4ba0cef0ed6fee
Evan Dorn <evan@lrdesign.com>
2011-07-26 14:42:12 -0700

    fixed spec problems with controllers resulting from last change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < AuthzController
   def index
     @user = current_user
-    @current_project = current_user.current_project
-    @work_units = current_user.work_units_for(@current_project).order("stop_time DESC").paginate(:per_page => 10, :page => params[:page])
+    if (@current_project = current_user.current_project )
+      @work_units = current_user.work_units_for(@current_project).order("stop_time DESC").paginate(:per_page => 10, :page => params[:page])
+    end
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,7 @@ describe HomeController do
 
   describe "logged in" do
     before :each do
-      authenticate(:user)
+      @user = authenticate(:user)
     end
 
     describe "GET 'index'" do
@@ -18,6 +18,26 @@ describe HomeController do
         controller.should be_authorized
       end
     end
+
+    describe "with a current project selected" do
+      before :each do
+        @user.current_project = Factory(:project, :billable => true, :clockable => true)
+        @user.save!
+      end
+      it "should find the current project" do
+        get 'index'
+        assigns[:current_project].should == @user.current_project
+      end
+
+      it "should find the completed work units" do
+        @wu1 = Factory(:work_unit, :project => @user.current_project, :user => @user)
+        @wu2 = Factory(:work_unit, :project => @user.current_project, :user => @user)
+        get 'index'
+        assigns[:work_units].should include(@wu1)
+        assigns[:work_units].should include(@wu2)
+      end
+    end
+
   end
 
   describe "logged out" do
