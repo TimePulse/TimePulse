050eb30d1be1f955b9a3957d2de5ab032137ce13
Evan Dorn <evan@lrdesign.com>
2011-07-28 13:31:12 -0700

    switch filter parameter logging to proper form

--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,7 @@ module Tracks
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation] unless Rails.env.development?
+
   end
 end
