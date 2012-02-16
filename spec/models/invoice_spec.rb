8f2800c22ce6a73058f3e8c0088ca74c7a16f4da
Tom Jakubowski <tom@crystae.net>
2012-02-15 20:19:46 -0800

    When an invoice is deleted, its work units' invoice attributes are set to nil

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -27,65 +27,65 @@ describe Invoice do
 
   it "should create a new instance given valid attributes" do
     Invoice.create!(@valid_attributes)
-  end        
-  
-  
+  end
+
+
   describe "hours" do
     it "should give the sum of the hours in contained work units" do
-      wus = [ 
+      wus = [
         Factory(:work_unit, :hours => 1.0),
         Factory(:work_unit, :hours => 2.0),
-        Factory(:work_unit, :hours => 3.0)                
+        Factory(:work_unit, :hours => 3.0)
       ]
-      Factory(:invoice, :work_units => wus).hours.should == 6.0      
+      Factory(:invoice, :work_units => wus).hours.should == 6.0
     end
   end
-  
+
   describe "overdue named scope" do
     it "should find a invoice due yesterday" do
-      @wu = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => nil)      
+      @wu = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => nil)
       Invoice.overdue.should include(@wu)
     end
     it "should not find a invoice due today" do
-      @wu = Factory(:invoice, :due_on => Date.today, :paid_on => nil)      
-      Invoice.overdue.should_not include(@wu)      
+      @wu = Factory(:invoice, :due_on => Date.today, :paid_on => nil)
+      Invoice.overdue.should_not include(@wu)
     end
     it "should not find a invoice due tomorrow" do
-      @wu = Factory(:invoice, :due_on => Date.today + 1.day, :paid_on => nil)      
-      Invoice.overdue.should_not include(@wu)      
+      @wu = Factory(:invoice, :due_on => Date.today + 1.day, :paid_on => nil)
+      Invoice.overdue.should_not include(@wu)
     end
     it "should not find a invoice due yesterday if it has been paid for" do
-      @wu = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => Date.today - 7.days)      
+      @wu = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => Date.today - 7.days)
       Invoice.overdue.should_not include(@wu)
     end
   end
-  
+
   describe "paid named scope" do
     before(:each) do
-      @paid_invoice = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => Date.today - 6.days)      
-      @unpaid_invoice = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => nil)      
+      @paid_invoice = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => Date.today - 6.days)
+      @unpaid_invoice = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => nil)
     end
     it "should find a paid invoice" do
-      Invoice.paid.should include(@paid_invoice)      
+      Invoice.paid.should include(@paid_invoice)
     end
     it "should not find an unpaid invoice" do
-      Invoice.paid.should_not include(@unpaid_invoice)      
-    end    
+      Invoice.paid.should_not include(@unpaid_invoice)
+    end
   end
-  
+
   describe "unpaid named scope" do
     before(:each) do
-      @paid_invoice = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => Date.today - 6.days)      
-      @unpaid_invoice = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => nil)      
+      @paid_invoice = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => Date.today - 6.days)
+      @unpaid_invoice = Factory(:invoice, :due_on => Date.today - 1.day, :paid_on => nil)
     end
     it "should not find a paid invoice" do
-      Invoice.unpaid.should_not include(@paid_invoice)      
+      Invoice.unpaid.should_not include(@paid_invoice)
     end
     it "should  find an unpaid invoice" do
-      Invoice.unpaid.should include(@unpaid_invoice)      
-    end    
+      Invoice.unpaid.should include(@unpaid_invoice)
+    end
   end
-  
+
   describe "work_unit_ids mass assignment" do
     it "should assign two work_units" do
       pending
@@ -95,6 +95,17 @@ describe Invoice do
       @invoice = Factory.build(:invoice, :work_unit_ids => { @wu1.id => "1", @wu2.id => "1" } )
     end
   end
-  
-  
+
+  describe "deleted invoice" do
+    it "should mark associated work units as uninvoiced" do
+      @proj = Factory(:project)
+      @wu = Factory(:work_unit, :project => @proj)
+      @invoice = Factory(:invoice, :work_units => [ @wu ])
+      @wu.invoice.should == @invoice
+      @invoice.destroy
+      @wu.invoice.should be_nil
+    end
+  end
+
+
 end
