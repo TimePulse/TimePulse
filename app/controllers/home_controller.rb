90a47fea47be10476a7c6499a88a45bc7bb89bfb
Evan Dorn <evan@lrdesign.com>
2013-02-01 12:00:08 -0800

    eager load an association for speed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < AuthzController
   def index
     @user = current_user
     if (@current_project = current_user.current_project )
-      @work_units = current_user.work_units_for(@current_project).order("stop_time DESC").paginate(:per_page => 10, :page => params[:page])
+      @work_units = current_user.work_units_for(@current_project).includes(:project => :client).order("stop_time DESC").paginate(:per_page => 10, :page => params[:page])
     end
   end
 end
