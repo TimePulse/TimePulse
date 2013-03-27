f2afeb587e493ea406503a82c457d4b67c24cf32
Evan Dorn <evan@lrdesign.com>
2013-03-27 15:07:58 -0700

    Implement dates on InvoiceReport

--- app/models/invoice_report.rb
+++ app/models/invoice_report.rb
@@ -1,14 +1,20 @@
 
 
 class InvoiceReport
-  attr_accessor :invoice
+  attr_accessor :invoice, :work_units
 
   def initialize(invoice)
     self.invoice = invoice
+    self.work_units = invoice.work_units
   end
 
   def users
-    invoice.work_units.all.map{|wu| wu.user}.uniq
-    #[]
+    work_units.all.map{|wu| wu.user}.uniq
   end
+
+  def days
+    work_units.all.map{ |wu| wu.stop_time.to_date }.uniq.sort
+  end
+
+
 end
--- spec/models/invoice_report_spec.rb
+++ spec/models/invoice_report_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe InvoiceReport do
 
-  describe "users" do
-    let :user_1 do Factory(:user) end
-    let :user_2 do Factory(:user) end
-    let :user_3 do Factory(:user) end
+  let :user_1 do Factory(:user) end
+  let :user_2 do Factory(:user) end
+  let :user_3 do Factory(:user) end
 
+  subject { InvoiceReport.new(invoice) }
+
+  describe "users" do
     let :work_unit_1 do
       Factory(:work_unit, :hours => 1.0, :user => user_1)
     end
@@ -21,10 +23,42 @@ describe InvoiceReport do
       Factory.create(:invoice, :work_units => [work_unit_1, work_unit_2])
     end
 
-    subject { InvoiceReport.new(invoice) }
 
     its(:users) { should include(user_1, user_2) }
     its(:users) { should_not include(user_3) }
+
+  end
+
+  describe "days" do
+    let :work_units do
+      [].tap do |arr|
+        5.times do |n|
+          start = n.days.ago.beginning_of_day
+          arr << Factory(:work_unit, :hours => 1.0,
+                         :start_time => start,
+                         :stop_time  => start + 1.hour)
+        end
+      end
+    end
+
+    let :invoice do
+      Factory.create(:invoice, :work_units => work_units)
+    end
+
+
+    #before do
+      #debugger
+      #'foo'
+     #end
+    its(:days) { should have(5).days }
+    its(:days) { should include(date_ago(4)) }
+    its(:days) { should include(date_ago(0)) }
+    its(:days) { should_not include(date_ago(5)) }
+
+
+    def date_ago(days)
+      days.days.ago.beginning_of_day.to_date
+    end
 
   end
 
