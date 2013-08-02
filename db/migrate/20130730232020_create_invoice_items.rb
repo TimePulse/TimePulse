40cb448e3c6ed154e3d0377d5b19954b0dff50e9
Evan Dorn <evan@lrdesign.com>
2013-08-02 14:44:49 -0700

    Fix column types in invoice_items

--- db/migrate/20130730232020_create_invoice_items.rb
+++ db/migrate/20130730232020_create_invoice_items.rb
@@ -2,9 +2,9 @@ class CreateInvoiceItems < ActiveRecord::Migration
   def change
     create_table :invoice_items do |t|
       t.string :name
-      t.float :amount
-      t.float :hours
-      t.float :total
+      t.decimal :amount, :precision => 8, :scale => 2
+      t.decimal :hours, :precision => 8, :scale => 2
+      t.decimal :total, :precision => 8, :scale => 2
 
       t.references :invoice
 
