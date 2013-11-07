878e06c690a349b7839cd232658af17f36730730
Evan Dorn <evan@lrdesign.com>
2013-11-06 16:40:46 -0800

    Convert application name to TimePulse
    [Finishes #60289698]

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Tracks::Application.load_tasks
+TimePulse::Application.load_tasks
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Tracks::Application.configure do
+TimePulse::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
