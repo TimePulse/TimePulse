50c4c87119c1bd64971bdc4e2c1084ab9e2fbd38
Evan Dorn <evan@lrdesign.com>
2013-11-06 17:13:23 -0800

    re-add deploy scripts

---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+set :domain, 'tracks.lrdesign.com'
+set :application, 'tracks'      # eg 'rfx'
+set :deploy_to, "/var/www/tracks.lrdesign.com"
+set :keep_releases, 10
+set :branch, 'production'
+set :rails_env, "production"
+set :use_sudo, false
