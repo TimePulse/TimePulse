cd66ee1649834369e332bc2075a0ac01750b0db1
Hannah Howard <hannah@hannahhoward.net>
2013-11-26 13:29:05 -0800

    Remove subclass data

--- app/controllers/work_units_controller.rb
+++ app/controllers/work_units_controller.rb
@@ -85,25 +85,6 @@ class WorkUnitsController < WorkUnitBaseController
     end
   end
 
-  def parse_date_params
-    if wu_p = params[:work_unit]
-      zone = Time.zone
-      if wu_p.has_key?(:time_zone)
-        zone = wu_p[:time_zone].to_i
-      end
-    Time.use_zone(zone) do
-        wu_p[:start_time] = parse_time(wu_p[:start_time]) if wu_p[:start_time]
-        wu_p[:stop_time] = parse_time(wu_p[:stop_time]) if wu_p[:stop_time]
-      end
-    end
-  end
-
-  def parse_time(string)
-    time_options = { :now => Time.zone.now, :context => :past }
-    Chronic.time_class = Time.zone
-    Chronic.parse(string, time_options)
-  end
-
   # compute a few fields based on sensible defaults, if "calculate" param was passed
   def compute_some_fields
     if params["work_unit"]["calculate"]
