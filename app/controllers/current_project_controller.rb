b087d2cb3ae5b8e50353cffc6cf0437c98017117
Hannah Howard <hannah@hannahhoward.net>
2013-12-30 16:52:05 -0800

    Fix current project setting

--- app/controllers/current_project_controller.rb
+++ app/controllers/current_project_controller.rb
@@ -1,11 +1,11 @@
-class CurrentProjectController < ApplicationController
+class CurrentProjectController < HomePageController
   before_filter :require_user!
 
   def create
     @prior_project = current_user.current_project
     current_user.update_attribute(:current_project_id, params[:id]) if params[:id]
     @current_project = current_user.reload.current_project
-    @work_units = current_user.completed_work_units_for(@current_project).order("stop_time DESC").paginate(:per_page => 10, :page => params[:page])
+    load_related_items
 
     respond_to do |format|
       format.html { redirect_to :back }
