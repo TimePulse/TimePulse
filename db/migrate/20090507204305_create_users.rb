5632ffb0e4f5f2a15419f6614c89c89967a04627
Evan Dorn <evan@lrdesign.com>
2014-01-15 15:33:31 -0800

    Remove two unnecessary/unused factories

--- db/migrate/20090507204305_create_users.rb
+++ db/migrate/20090507204305_create_users.rb
@@ -1,10 +1,12 @@
+class User < ActiveRecord::Base
+end
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string    :login,               :null => false                # optional, you can use email instead, or both
       t.string    :email,               :null => false                # optional, you can use login instead, or both
       t.references :current_project,    :null => true
-      t.string    :name,                :null => false      
+      t.string    :name,                :null => false
 
       t.string    :crypted_password,    :null => false                # optional, see below
       t.string    :password_salt,       :null => false                # optional, but highly recommended
--- spec/factories/logical_authz_groups.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define  do
-  factory :group do
-    sequence(:name) {|n| "group_#{n}"}
-  end
-  factory :admin_group, :parent => :group do
-    name "Administration"
-  end
-end
-
