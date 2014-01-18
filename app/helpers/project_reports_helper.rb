14b6cc3d7ba02b295598387d4c0999d80eb2ce82
Joe <jboninger@gmail.com>
2014-01-17 16:01:06 -0800

    Finish work on first version of project report controller

--- app/helpers/project_reports_helper.rb
+++ app/helpers/project_reports_helper.rb
@@ -4,32 +4,15 @@ module ProjectReportsHelper
     select_tag(:project_id, options_for_select(project_options), { :include_blank => "", :class => "project_selector"})
   end
 
-  def user_project_hours
-    total = 0
-    @user_hours = []
-    @work_units.each do |wu|
-      in_list = false
-      user = User.find_by_id(wu.user_id)
-      
-      @user_hours.each do |uh|
-        if user.name == uh[0]
-          in_list = true
-          uh[1] += wu.hours
-          break
-        end
-      end
-      
-      if in_list == false
-        @user_hours.push([user.name, wu.hours])
-      end
-      
-      total += wu.hours
-      
+  def user_project_hours(work_units)
+    #byebug
+    user_hours = {}
+    work_units.each do |wu|
+      user_hours[wu.user] ||= 0.0
+      user_hours[wu.user] += wu.hours
     end
-    
-    @user_hours.push(["Total", total])
-    
-    return @user_hours
+    user_hours["Total"] = user_hours.values.reduce(0){ |sum, wu| sum += wu }
+    user_hours
   end
   
   def report_title
