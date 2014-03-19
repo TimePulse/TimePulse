4b98e5348dab18ecaa2fd7e75c6778c529aad9ba
Evan Dorn <evan@lrdesign.com>
2014-03-18 17:04:03 -0700

    Remove duplicate method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,12 +9,4 @@ class HomeController < HomePageController
 
   end
 
-  private
-
-  def related_items_for(property, order_prop, page)
-    related_items = current_user.send((property.to_s + "_for").to_sym, @current_project)
-    related_items = related_items.includes(:project => :client)
-    related_items = related_items.order(order_prop.to_s + " DESC")
-    related_items = related_items.paginate(:per_page => 10, :page => page)
-  end
 end
