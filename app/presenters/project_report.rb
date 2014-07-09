87823e1837fc71e98dc0afa206876e02c4a598c6
Christa Hartsock <christa.hartsock@gmail.com>
2014-07-08 18:27:20 -0700

    create projectreport presenter

---
+++ app/presenters/project_report.rb
@@ -0,0 +1,13 @@
+class ProjectReport
+	def initialize(project)
+		# 
+	end
+
+	def work_units_by_user
+		# return 2d array, each array is single row for user
+		# contains username, hours, $$$
+	end
+
+	def work_units
+	end
+end
