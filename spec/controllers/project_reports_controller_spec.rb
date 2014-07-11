9706a31a9b79699565a5df3075d1e050431c5469
Hannah Howard <hannah@hannahhoward.net>
2014-07-11 16:54:27 -0700

    Fix tab to spaces

--- spec/controllers/project_reports_controller_spec.rb
+++ spec/controllers/project_reports_controller_spec.rb
@@ -5,49 +5,49 @@ describe ProjectReportsController do
     @project = FactoryGirl.create(:project)
   end
 
-	describe "GET new" do
-		describe "accessed by an admin" do
-			before do
-	      authenticate(:admin)
-	    end
+  describe "GET new" do
+    describe "accessed by an admin" do
+      before do
+        authenticate(:admin)
+      end
 
-	    it "should be authorized" do
-	    	get :new
+      it "should be authorized" do
+        get :new
         verify_authorization_successful
-	    end
+      end
 
-	    it "assigns all projects as @projects" do
-	    	get :new
-	    	expect(assigns[:projects]).to include(@project)
-	    end
+      it "assigns all projects as @projects" do
+        get :new
+        expect(assigns[:projects]).to include(@project)
+      end
 
-	    describe "has a project_id" do
-	    	let :project_report do
-	    		double(ProjectReport)
-	    	end
+      describe "has a project_id" do
+        let :project_report do
+          double(ProjectReport)
+        end
 
-	    	before do
-	    		ProjectReport.should_receive(:new).and_return(project_report)
-	    		get :new, :project_id => @project.id
-	    	end
+        before do
+          ProjectReport.should_receive(:new).and_return(project_report)
+          get :new, :project_id => @project.id
+        end
 
-	    	it "should assign a project as @project" do
-	    		expect(assigns[:project]).to eq(@project)
-	    	end
+        it "should assign a project as @project" do
+          expect(assigns[:project]).to eq(@project)
+        end
 
-	    	it "creates a project report" do
-	    		expect(assigns[:project_report]).to eq(project_report)
-	    	end
-	    end
+        it "creates a project report" do
+          expect(assigns[:project_report]).to eq(project_report)
+        end
+      end
 
-	  end
+    end
 
-	  describe "accessed by not an admin" do
-	  	it "should not be authorized" do
+    describe "accessed by not an admin" do
+      it "should not be authorized" do
         get :new
         verify_authorization_unsuccessful
-	  	end
-	  end
-	end
-	
+      end
+    end
+  end
+
 end
