c0ed3316ff970aa8b4d156dc39c932198d95aff5
Christa Hartsock <christa.hartsock@gmail.com>
2014-07-11 17:11:11 -0700

    project report refactor

--- app/presenters/project_report.rb
+++ app/presenters/project_report.rb
@@ -1,53 +1,72 @@
 class ProjectReport
 	def initialize(project)
 		@project = project
-		@user_hours = user_hours
-		@users = users
 	end
 
-	attr_reader :project, :user_hours, :users
+	attr_reader :project, :rates
 
 	def users(scope = self.work_units)
 		scope.to_a.map{|pwu| pwu.user}.uniq
 	end
 
+	def rates
+		@project.rates.uniq
+	end
+
+	def by_hour_hash(&block)
+		by_hours = {}
+		self.work_units.each do |wu|
+			model = yield(wu)
+			if model
+				key = model.id
+				by_hours[key] ||= 0
+				by_hours[key] += wu.hours
+			end
+		end
+		by_hours
+	end
+
 	def user_hours
-		user_hours = {}
-    self.work_units.each do |wu|
-      user_hours[wu.user.id] ||= 0.0
-      user_hours[wu.user.id] += wu.hours
-    end
-    user_hours
+		by_hours_hash { |wu| wu.user }
+	end
+
+	def rate_hours
+		by_hours_hash { |wu| wu.rate }
 	end
 
 	def work_units
 		@work_units = WorkUnit.for_project(@project).completed.billable.uninvoiced.flatten.uniq
 	end
 
+	def build_report_table()
+	end
+
 	def build_user_report
-		@rows = Hash.new
+		user_hours = self.user_hours
 
-		@users.each do |user|
-			rate = user.rate_for(@project).amount
+		rows = Hash.new
 
-			fields = Hash[:name => user.name, :hours => @user_hours[user.id], :rate => rate, :cost => (@user_hours[user.id] * rate)]
-			@rows[user.id] = fields
+		self.users.each do |user|
+			rate = user.rate_for(@project)
+
+			fields = Hash[:name => user.name, :hours => user_hours[user.id], :rate => rate.amount, :cost => (user_hours[user.id] * rate.amount)]
+			rows[user.id] = fields
 		end
 
-		@rows
+		rows
 	end
 
 	def build_rate_report
-		@rows = Hash.new
+		rate_hours = self.rate_hours
 
-		@users.each do |user|
-			rate = user.rate_for(@project).amount
+		rows = Hash.new
 
-			fields = Hash[:name => user.name, :hours => @user_hours[user.id], :rate => rate, :cost => (@user_hours[user.id] * rate)]
-			@rows[user.id] = fields
+		self.rates.each do |rate|
+			fields = Hash[:name => rate.name, :hours => rate_hours[rate.id], :rate => rate.amount, :cost => (rate_hours[rate.id] * rate.amount)]
+			rows[rate.id] = fields
 		end
 
-		@rows
+		rows
 	end
 
 end
