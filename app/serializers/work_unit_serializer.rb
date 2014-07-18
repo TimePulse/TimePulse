fbd178d325313856389d0abcca3abdd59d0a5129
Judson <nyarly@gmail.com>
2014-07-17 17:37:01 -0700

    Updating production cap config

--- app/serializers/work_unit_serializer.rb
+++ app/serializers/work_unit_serializer.rb
@@ -1,4 +1,3 @@
 class WorkUnitSerializer < ActiveModel::Serializer
   attributes :id, :start_time, :stop_time, :hours, :notes, :billable
 end
-
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,10 @@
-set :domain, 'tracks.lrdesign.com'
+set :runner, 'apache'
+set :user, 'root'
+
+set :domain, 'appserver2.lrdesign.com'
 set :application, 'tracks'      # eg 'rfx'
 set :deploy_to, "/var/www/tracks.lrdesign.com"
+
 set :keep_releases, 10
 set :branch, 'production'
 set :rails_env, "production"
