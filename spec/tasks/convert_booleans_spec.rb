380595f30d41dff0dc4120c8b5adc922d0cd7e4a
Christa Hartsock <christa.hartsock@gmail.com>
2014-09-03 20:09:39 -0700

    rake task built

--- spec/tasks/convert_booleans_spec.rb
+++ spec/tasks/convert_booleans_spec.rb
@@ -3,18 +3,17 @@ require 'rake'
 
 describe 'db:convert_to_boolean namespace rake task', :type => :task do
 
-  steps 'db:create_bool_columns' do
+  steps 'db:create_temp_columns' do
 
     before do
       load File.expand_path("../../../lib/tasks/convert_booleans.rake", __FILE__)
       Rake::Task.define_task(:environment)
+      Rake::Task.define_task(:to_bool)
     end
 
     it 'should succeed' do
-      Rake::Task["db:revert_bool_columns"].reenable
-      Rake::Task["db:revert_bool_columns"].invoke
-      Rake::Task["db:create_bool_columns"].reenable
-      Rake::Task["db:create_bool_columns"].invoke
+      Rake::Task["db:create_temp_columns"].reenable
+      Rake::Task["db:create_temp_columns"].invoke
     end
 
     describe Project do
@@ -34,4 +33,45 @@ describe 'db:convert_to_boolean namespace rake task', :type => :task do
     end
   end
 
+  steps 'db:convert_columns' do
+    # before do
+    #   load File.expand_path("../../../lib/tasks/convert_booleans.rake", __FILE__)
+    #   Rake::Task.define_task(:environment)
+    # end
+    # it 'should succeed' do
+    #   Rake::Task["db:create_temp_columns"].reenable
+    #   Rake::Task["db:create_temp_columns"].invoke
+    # end
+
+  end
+
+  steps 'db:delete_temp_columns' do
+
+    before do
+      load File.expand_path("../../../lib/tasks/convert_booleans.rake", __FILE__)
+      Rake::Task.define_task(:environment)
+      Rake::Task.define_task(:to_bool)
+    end
+
+    it 'should succeed' do
+      Rake::Task["db:delete_temp_columns"].reenable
+      Rake::Task["db:delete_temp_columns"].invoke
+    end
+
+    describe Project do
+      it {should_not have_db_column(:clockable_new)}
+      it {should_not have_db_column(:billable_new)}
+      it {should_not have_db_column(:flat_rate_new)}
+      it {should_not have_db_column(:archived_new)}
+    end
+
+    describe User do
+      it {should_not have_db_column(:inactive_new)}
+      it {should_not have_db_column(:admin_new)}
+    end
+
+    describe WorkUnit do
+      it {should_not have_db_column(:billable_new)}
+    end
+  end
 end
