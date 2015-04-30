3db3870a140303defc61377df0727d7346a19b89
brookekfox <brookekfox@gmail.com>
2015-04-29 17:39:58 -0700

    added date picker and integration tests for it
    [#93493736]

--- app/assets/javascripts/ninjascripts/hours_reports.js
+++ app/assets/javascripts/ninjascripts/hours_reports.js
@@ -4,6 +4,10 @@ function showHours(category) {
 	$('span.unbillable').hide();
 	$('span.' + category).show();
 }
+function formatDate(date) {
+	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	return monthNames[date.getMonth()] + ' ' + date.getDate() + ' ' + date.getFullYear().toString().split('')[2] + date.getFullYear().toString().split('')[3];
+}
 
 Ninja.behavior({
 	'#total-user-hours-btn': {
@@ -26,6 +30,17 @@ Ninja.behavior({
 			$('span.total').show();
 			$('span.billable').show();
 			$('span.unbillable').show();
+		}
+	},
+	'#new-dates-btn': {
+		click: function() {
+			var end_date = $('#datepicker').datepicker('getDate');
+			$('h2#sunday_5').html('Week ending ' + formatDate(end_date));
+			$('h2#sunday_4').html('Week ending ' + formatDate(new Date(end_date.setDate(end_date.getDate() - 7))));
+			$('h2#sunday_3').html('Week ending ' + formatDate(new Date(end_date.setDate(end_date.getDate() - 7))));
+			$('h2#sunday_2').html('Week ending ' + formatDate(new Date(end_date.setDate(end_date.getDate() - 7))));
+			$('h2#sunday_1').html('Week ending ' + formatDate(new Date(end_date.setDate(end_date.getDate() - 7))));
+			$('h2#sunday_0').html('Week ending ' + formatDate(new Date(end_date.setDate(end_date.getDate() - 7))));
 		}
 	}
 });
