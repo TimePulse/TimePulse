1fa0e461027f2824e0fea67e936aa9377cef4009
brookekfox <brookekfox@gmail.com>
2015-04-30 16:25:02 -0700

    work unit query spec passes
    [#93493736]

--- app/queries/work_unit_query.rb
+++ app/queries/work_unit_query.rb
@@ -1,17 +1,17 @@
 class WorkUnitQuery
 
-	def initialize(user, chosen_day, num_weeks_ago, scope)
+	def initialize(user, start_date, num_weeks_ago, scope)
 		@user = user
 		@num_weeks_ago = num_weeks_ago
 		@scope = scope
-		@chosen_day = chosen_day
+		@start_date = start_date
 	end
 
 	def hours
 		if @scope == 'total'
-			@user.work_units.where(:start_time => @chosen_day.beginning_of_week - 1.second - ((@num_weeks_ago * 7) - 1).days..@chosen_day.beginning_of_week - 1.second - ((@num_weeks_ago - 1) * 7).days).sum(:hours).to_s.to_f
+			@user.work_units.where(:start_time => @start_date.beginning_of_week - 1.second - ((@num_weeks_ago * 7) - 1).days..@start_date.beginning_of_week - 1.second - ((@num_weeks_ago - 1) * 7).days).sum(:hours).to_s.to_f
 		elsif @scope == 'billable' || @scope == 'unbillable'
-			@user.work_units.where(:start_time => @chosen_day.beginning_of_week - 1.second - ((@num_weeks_ago * 7) - 1).days..@chosen_day.beginning_of_week - 1.second - ((@num_weeks_ago - 1) * 7).days).send(@scope.to_sym).sum(:hours).to_s.to_f
+			@user.work_units.where(:start_time => @start_date.beginning_of_week - 1.second - ((@num_weeks_ago * 7) - 1).days..@start_date.beginning_of_week - 1.second - ((@num_weeks_ago - 1) * 7).days).send(@scope.to_sym).sum(:hours).to_s.to_f
 		end
 	end
 
--- spec/queries/work_unit_query_spec.rb
+++ spec/queries/work_unit_query_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe WorkUnitQuery, :type => :query do
 
+	before do
+		Timecop.travel(Time.local(2015, 4, 27, 0, 0, 0))
+	end
+
 	before :each do
 		@user_1 = FactoryGirl.create(:user, :name => 'Foo Bar 1')
 		@user_2 = FactoryGirl.create(:user, :name => 'Foo Bar 2')
@@ -44,11 +48,11 @@ describe WorkUnitQuery, :type => :query do
 													 :start_time => Time.now - 7.weeks,
 													 :billable => false)
 		]
-		@billable_work_unit_1 = WorkUnitQuery.new(@user_1,2,'billable')
-		@billable_work_unit_2 = WorkUnitQuery.new(@user_2,2,'billable')
-		@unbillable_work_unit_1 = WorkUnitQuery.new(@user_1,2,'unbillable')
-		@unbillable_work_unit_2 = WorkUnitQuery.new(@user_1,3,'unbillable')
-		@total_work_unit_1 = WorkUnitQuery.new(@user_1,3,'total')
+		@billable_work_unit_1 = WorkUnitQuery.new(@user_1,Time.now,2,'billable')
+		@billable_work_unit_2 = WorkUnitQuery.new(@user_2,Time.now,2,'billable')
+		@unbillable_work_unit_1 = WorkUnitQuery.new(@user_1,Time.now,2,'unbillable')
+		@unbillable_work_unit_2 = WorkUnitQuery.new(@user_1,Time.now,3,'unbillable')
+		@total_work_unit_1 = WorkUnitQuery.new(@user_1,Time.now,3,'total')
 	end
 
 	it "should find the recent billable hours" do
@@ -63,5 +67,9 @@ describe WorkUnitQuery, :type => :query do
 
 	it "should find the recent total hours" do
 		expect(@total_work_unit_1.hours).to eq(10)
+	end
+
+	after do
+		Timecop.return
 	end
 end
