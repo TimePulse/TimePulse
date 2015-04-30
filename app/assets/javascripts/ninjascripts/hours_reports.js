62ebb4f20516f184e3ff9bd4b3a640c536637a9d
brookekfox <brookekfox@gmail.com>
2015-04-30 14:42:10 -0700

    date pickers on one line, color highlighting for buttons (billable, unbillable, total, all)
    [#93493736]

--- app/assets/javascripts/ninjascripts/hours_reports.js
+++ app/assets/javascripts/ninjascripts/hours_reports.js
@@ -1,8 +1,8 @@
 function showHours(category) {
-	$('span.total').hide();
-	$('span.billable').hide();
-	$('span.unbillable').hide();
-	$('span.' + category).show();
+	$('div.total').hide();
+	$('div.billable').hide();
+	$('div.unbillable').hide();
+	$('div.' + category).show();
 }
 function formatDate(date) {
 	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -13,23 +13,42 @@ Ninja.behavior({
 	'#total-user-hours-btn': {
 		click: function() {
 			showHours('total');
+			//$('#total-user-hours-btn').css("font-style","italic");
+			$('#total-user-hours-btn').css("background-color","#165D75");
+			$('#all-user-hours-btn').css("background-color","#2BA6CB");
+			$('#billable-user-hours-btn').css("background-color","#2BA6CB");
+			$('#unbillable-user-hours-btn').css("background-color","#2BA6CB");
 		}
 	},
 	'#billable-user-hours-btn': {
 		click: function() {
 			showHours('billable');
+			//$('#billable-user-hours-btn').css("font-style","italic");
+			$('#billable-user-hours-btn').css("background-color","#165D75");
+			$('#all-user-hours-btn').css("background-color","#2BA6CB");
+			$('#total-user-hours-btn').css("background-color","#2BA6CB");
+			$('#unbillable-user-hours-btn').css("background-color","#2BA6CB");
 		}
 	},
 	'#unbillable-user-hours-btn': {
 		click: function() {
 			showHours('unbillable');
+			//$('#unbillable-user-hours-btn').css("font-style","italic");
+			$('#unbillable-user-hours-btn').css("background-color","#165D75");
+			$('#all-user-hours-btn').css("background-color","#2BA6CB");
+			$('#billable-user-hours-btn').css("background-color","#2BA6CB");
+			$('#total-user-hours-btn').css("background-color","#2BA6CB");
 		}
 	},
 	'#all-user-hours-btn': {
 		click: function() {
-			$('span.total').show();
-			$('span.billable').show();
-			$('span.unbillable').show();
+			$('div.total').show();
+			$('div.billable').show();
+			$('div.unbillable').show();
+			$('#all-user-hours-btn').css("background-color","#165D75");
+			$('#unbillable-user-hours-btn').css("background-color","#2BA6CB");
+			$('#billable-user-hours-btn').css("background-color","#2BA6CB");
+			$('#total-user-hours-btn').css("background-color","#2BA6CB");
 		}
 	}
 });
