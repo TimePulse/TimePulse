279a3f52444c873399f174a55627c74d21994574
brookekfox <brookekfox@gmail.com>
2015-05-01 10:03:43 -0700

    converted indents to spaces
    [#93493736]

--- spec/stories/admin_views_hours_reports_spec.rb
+++ spec/stories/admin_views_hours_reports_spec.rb
@@ -10,36 +10,36 @@ steps "Admin views the hours reports", :type => :feature do
   let! :user_2 do FactoryGirl.create(:user, :name => "Foo Bar 2") end
   let! :project_1 do FactoryGirl.create(:project) end
   let! :project_2 do FactoryGirl.create(:project) end
-	let! :work_unit_0 do
-		FactoryGirl.create(:work_unit, :hours => 10, :user => user_2, :project => project_2, :start_time => Time.now - 124.days, :stop_time => Time.now - 123.days, :billable => false)
-	end
+  let! :work_unit_0 do
+    FactoryGirl.create(:work_unit, :hours => 10, :user => user_2, :project => project_2, :start_time => Time.now - 124.days, :stop_time => Time.now - 123.days, :billable => false)
+  end
   let! :work_unit_1 do
-		FactoryGirl.create(:work_unit, :hours => 9, :user => user_2, :project => project_2, :start_time => Time.now - 122.days, :stop_time => Time.now - 121.days)
-	end
-	let! :work_unit_2 do
-		FactoryGirl.create(:work_unit, :hours => 8, :user => user_2, :project => project_2, :start_time => Time.now - 115.days, :stop_time => Time.now - 114.days, :billable => false)
-	end
-	let! :work_unit_3 do
-		FactoryGirl.create(:work_unit, :hours => 7, :user => user_2, :project => project_2, :start_time => Time.now - 108.days, :stop_time => Time.now - 107.days)
-	end
+    FactoryGirl.create(:work_unit, :hours => 9, :user => user_2, :project => project_2, :start_time => Time.now - 122.days, :stop_time => Time.now - 121.days)
+  end
+  let! :work_unit_2 do
+    FactoryGirl.create(:work_unit, :hours => 8, :user => user_2, :project => project_2, :start_time => Time.now - 115.days, :stop_time => Time.now - 114.days, :billable => false)
+  end
+  let! :work_unit_3 do
+    FactoryGirl.create(:work_unit, :hours => 7, :user => user_2, :project => project_2, :start_time => Time.now - 108.days, :stop_time => Time.now - 107.days)
+  end
   let! :work_unit_4 do
-		FactoryGirl.create(:work_unit, :hours => 6, :user => user_1, :project => project_1, :start_time => Time.now - 37.days, :stop_time => Time.now - 36.days)
+    FactoryGirl.create(:work_unit, :hours => 6, :user => user_1, :project => project_1, :start_time => Time.now - 37.days, :stop_time => Time.now - 36.days)
   end
   let! :work_unit_5 do
     FactoryGirl.create(:work_unit, :hours => 5, :user => user_1, :project => project_1, :start_time => Time.now - 30.days, :stop_time => Time.now - 29.days, :billable => false)
-	end
-	let! :work_unit_6 do
-		FactoryGirl.create(:work_unit, :hours => 4, :user => user_1, :project => project_1, :start_time => Time.now - 23.days, :stop_time => Time.now - 22.days, :billable => false)
-	end
-	let! :work_unit_7 do
-		FactoryGirl.create(:work_unit, :hours => 3, :user => user_1, :project => project_1, :start_time => Time.now - 16.days, :stop_time => Time.now - 15.days)
-	end
-	let! :work_unit_8 do
-		FactoryGirl.create(:work_unit, :hours => 2, :user => user_1, :project => project_1, :start_time => Time.now - 9.days, :stop_time => Time.now - 8.days)
-	end
-	let! :work_unit_9 do
-		FactoryGirl.create(:work_unit, :hours => 1, :user => user_1, :project => project_1, :start_time => Time.now - 2.days, :stop_time => Time.now - 1.day)
-	end
+  end
+  let! :work_unit_6 do
+    FactoryGirl.create(:work_unit, :hours => 4, :user => user_1, :project => project_1, :start_time => Time.now - 23.days, :stop_time => Time.now - 22.days, :billable => false)
+  end
+  let! :work_unit_7 do
+    FactoryGirl.create(:work_unit, :hours => 3, :user => user_1, :project => project_1, :start_time => Time.now - 16.days, :stop_time => Time.now - 15.days)
+  end
+  let! :work_unit_8 do
+    FactoryGirl.create(:work_unit, :hours => 2, :user => user_1, :project => project_1, :start_time => Time.now - 9.days, :stop_time => Time.now - 8.days)
+  end
+  let! :work_unit_9 do
+    FactoryGirl.create(:work_unit, :hours => 1, :user => user_1, :project => project_1, :start_time => Time.now - 2.days, :stop_time => Time.now - 1.day)
+  end
   let! :admin do FactoryGirl.create(:admin) end
 
   it 'should login as the admin' do
@@ -74,157 +74,157 @@ steps "Admin views the hours reports", :type => :feature do
     page.should_not have_content("Foo Bar 2")
   end
 
-	it "should show billable, nonbillable, and total work units for one week ago" do
+  it "should show billable, nonbillable, and total work units for one week ago" do
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 7.days..Time.now.beginning_of_week - 1.second).sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 7.days..Time.now.beginning_of_week - 1.second).billable.sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 7.days..Time.now.beginning_of_week - 1.second).unbillable.sum(:hours).to_s.to_f)
-	end
+  end
 
-	it "should show billable, nonbillable, and total work units for two weeks ago" do
+  it "should show billable, nonbillable, and total work units for two weeks ago" do
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 13.days..Time.now.beginning_of_week - 1.second - 7.days).sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 13.days..Time.now.beginning_of_week - 1.second - 7.days).billable.sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 13.days..Time.now.beginning_of_week - 1.second - 7.days).unbillable.sum(:hours).to_s.to_f)
-	end
+  end
 
-	it "should show billable, nonbillable, and total work units for three weeks ago" do
+  it "should show billable, nonbillable, and total work units for three weeks ago" do
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 20.days..Time.now.beginning_of_week - 1.second - 14.days).sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 20.days..Time.now.beginning_of_week - 1.second - 14.days).billable.sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 20.days..Time.now.beginning_of_week - 1.second - 14.days).unbillable.sum(:hours).to_s.to_f)
-	end
+  end
 
-	it "should show billable, nonbillable, and total work units for four weeks ago" do
+  it "should show billable, nonbillable, and total work units for four weeks ago" do
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 27.days..Time.now.beginning_of_week - 1.second - 21.days).sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 27.days..Time.now.beginning_of_week - 1.second - 21.days).billable.sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 27.days..Time.now.beginning_of_week - 1.second - 21.days).unbillable.sum(:hours).to_s.to_f)
-	end
+  end
 
-	it "should show billable, nonbillable, and total work units for five weeks ago" do
+  it "should show billable, nonbillable, and total work units for five weeks ago" do
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 34.days..Time.now.beginning_of_week - 1.second - 28.days).sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 34.days..Time.now.beginning_of_week - 1.second - 28.days).billable.sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 34.days..Time.now.beginning_of_week - 1.second - 28.days).unbillable.sum(:hours).to_s.to_f)
-	end
+  end
 
-	it "should show billable, nonbillable, and total work units for six weeks ago" do
+  it "should show billable, nonbillable, and total work units for six weeks ago" do
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 41.days..Time.now.beginning_of_week - 1.second - 35.days).sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 41.days..Time.now.beginning_of_week - 1.second - 35.days).billable.sum(:hours).to_s.to_f)
    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 41.days..Time.now.beginning_of_week - 1.second - 35.days).unbillable.sum(:hours).to_s.to_f)
-	end
+  end
 
-	it "should click the 'total' button" do
-		find('#total-user-hours-btn').trigger('click')
-	end
+  it "should click the 'total' button" do
+    find('#total-user-hours-btn').trigger('click')
+  end
 
-	it "should show only the total number of hours for each user" do
-		page.should have_content("1.0")
-		page.should have_content("2.0")
-		page.should have_content("3.0")
-		page.should have_content("4.0")
-		page.should have_content("5.0")
-		page.should have_content("6.0")
-	end
+  it "should show only the total number of hours for each user" do
+    page.should have_content("1.0")
+    page.should have_content("2.0")
+    page.should have_content("3.0")
+    page.should have_content("4.0")
+    page.should have_content("5.0")
+    page.should have_content("6.0")
+  end
 
-	it "should click the 'billable' button" do
-		find('#billable-user-hours-btn').trigger('click')
-	end
+  it "should click the 'billable' button" do
+    find('#billable-user-hours-btn').trigger('click')
+  end
 
-	it "should show only the total number of billable hours for each user" do
-		page.should have_content("1.0")
-		page.should have_content("2.0")
-		page.should have_content("3.0")
-		page.should have_content("6.0")
-		page.should_not have_content("4.0")
-		page.should_not have_content("5.0")
-	end
+  it "should show only the total number of billable hours for each user" do
+    page.should have_content("1.0")
+    page.should have_content("2.0")
+    page.should have_content("3.0")
+    page.should have_content("6.0")
+    page.should_not have_content("4.0")
+    page.should_not have_content("5.0")
+  end
 
-	it "should click the 'unbillable' button" do
-		find('#unbillable-user-hours-btn').trigger('click')
-	end
+  it "should click the 'unbillable' button" do
+    find('#unbillable-user-hours-btn').trigger('click')
+  end
 
-	it "should show only the total number of unbillable hours for each user" do
-		page.should have_content("4.0")
-		page.should have_content("5.0")
-		page.should have_content("0.0")
-		page.should_not have_content("1.0")
-		page.should_not have_content("2.0")
-		page.should_not have_content("3.0")
-		page.should_not have_content("6.0")
-	end
+  it "should show only the total number of unbillable hours for each user" do
+    page.should have_content("4.0")
+    page.should have_content("5.0")
+    page.should have_content("0.0")
+    page.should_not have_content("1.0")
+    page.should_not have_content("2.0")
+    page.should_not have_content("3.0")
+    page.should_not have_content("6.0")
+  end
 
-	it "should click the 'billable' button" do
-		find('#billable-user-hours-btn').trigger('click')
-	end
+  it "should click the 'billable' button" do
+    find('#billable-user-hours-btn').trigger('click')
+  end
 
-	it "should show only the billable hours for each user" do
-		page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 7.days..Time.now.beginning_of_week - 1.second).billable.sum(:hours).to_s.to_f)
-		page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 20.days..Time.now.beginning_of_week - 1.second - 14.days).billable.sum(:hours).to_s.to_f)
-		page.should_not have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 27.days..Time.now.beginning_of_week - 1.second - 21.days).unbillable.sum(:hours).to_s.to_f)
-		page.should_not have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 34.days..Time.now.beginning_of_week - 1.second - 28.days).sum(:hours).to_s.to_f)
-	end
+  it "should show only the billable hours for each user" do
+    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 7.days..Time.now.beginning_of_week - 1.second).billable.sum(:hours).to_s.to_f)
+    page.should have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 20.days..Time.now.beginning_of_week - 1.second - 14.days).billable.sum(:hours).to_s.to_f)
+    page.should_not have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 27.days..Time.now.beginning_of_week - 1.second - 21.days).unbillable.sum(:hours).to_s.to_f)
+    page.should_not have_content(user_1.work_units.where(:start_time => Time.now.beginning_of_week - 1.second - 34.days..Time.now.beginning_of_week - 1.second - 28.days).sum(:hours).to_s.to_f)
+  end
 
-	it "should change the date range" do
-		fill_in "start-datepicker", :with => "01/01/2015"
-		fill_in "end-datepicker", :with => "01/15/2015"
-		find('#datepicker-submit-btn').trigger('click')
-	end
+  it "should change the date range" do
+    fill_in "start-datepicker", :with => "01/01/2015"
+    fill_in "end-datepicker", :with => "01/15/2015"
+    find('#datepicker-submit-btn').trigger('click')
+  end
 
-	it "should show the Sundays between the start and end dates as column headers" do
-		page.should have_content("Dec 28 14")
-		page.should have_content("Jan 04 15")
-		page.should have_content("Jan 11 15")
-	end
+  it "should show the Sundays between the start and end dates as column headers" do
+    page.should have_content("Dec 28 14")
+    page.should have_content("Jan 04 15")
+    page.should have_content("Jan 11 15")
+  end
 
-	it "should show users with work units in the last six weeks as rows" do
-		page.should have_content("Foo Bar 2")
-	end
+  it "should show users with work units in the last six weeks as rows" do
+    page.should have_content("Foo Bar 2")
+  end
 
-	it "should not show users with work units not in the last six weeks" do
-		page.should_not have_content("Foo Bar 1")
-	end
+  it "should not show users with work units not in the last six weeks" do
+    page.should_not have_content("Foo Bar 1")
+  end
 
-	it "should show total, billable, and unbillable work units for the last week" do
-		page.should have_content("19.0")
-		page.should have_content("10.0")
-		page.should have_content("9.0")
-		page.should have_content("8.0")
-		page.should have_content("7.0")
-		page.should have_content("0.0")
-	end
+  it "should show total, billable, and unbillable work units for the last week" do
+    page.should have_content("19.0")
+    page.should have_content("10.0")
+    page.should have_content("9.0")
+    page.should have_content("8.0")
+    page.should have_content("7.0")
+    page.should have_content("0.0")
+  end
 
-	it "should click the 'total' button" do
-		find('#total-user-hours-btn').trigger('click')
-	end
+  it "should click the 'total' button" do
+    find('#total-user-hours-btn').trigger('click')
+  end
 
-	it "should show only the total number of hours for each user" do
-		page.should have_content("19.0")
-		page.should have_content("8.0")
-		page.should have_content("7.0")
-		page.should_not have_content("10.0")
-		page.should_not have_content("0.0")
-	end
+  it "should show only the total number of hours for each user" do
+    page.should have_content("19.0")
+    page.should have_content("8.0")
+    page.should have_content("7.0")
+    page.should_not have_content("10.0")
+    page.should_not have_content("0.0")
+  end
 
-	it "should click the 'billable' button" do
-		find('#billable-user-hours-btn').trigger('click')
-	end
+  it "should click the 'billable' button" do
+    find('#billable-user-hours-btn').trigger('click')
+  end
 
-	it "should show only the total number of billable hours for each user" do
-		page.should have_content("9.0")
-		page.should have_content("7.0")
-		page.should have_content("0.0")
-		page.should_not have_content("8.0")
-		page.should_not have_content("10.0")
-	end
+  it "should show only the total number of billable hours for each user" do
+    page.should have_content("9.0")
+    page.should have_content("7.0")
+    page.should have_content("0.0")
+    page.should_not have_content("8.0")
+    page.should_not have_content("10.0")
+  end
 
-	it "should click the 'unbillable' button" do
-		find('#unbillable-user-hours-btn').trigger('click')
-	end
+  it "should click the 'unbillable' button" do
+    find('#unbillable-user-hours-btn').trigger('click')
+  end
 
-	it "should show only the total number of unbillable hours for each user" do
-		page.should_not have_content("9.0")
-		page.should_not have_content("7.0")
-		page.should have_content("0.0")
-		page.should have_content("8.0")
-		page.should have_content("10.0")
-	end
+  it "should show only the total number of unbillable hours for each user" do
+    page.should_not have_content("9.0")
+    page.should_not have_content("7.0")
+    page.should have_content("0.0")
+    page.should have_content("8.0")
+    page.should have_content("10.0")
+  end
 
   after do
     Timecop.return
