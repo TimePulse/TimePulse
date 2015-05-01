8286eba35a189b05f50f033b7571a491d3c80295
brookekfox <brookekfox@gmail.com>
2015-04-30 18:20:32 -0700

    converted tabs to spaces
    [#93493736]

--- app/controllers/hours_reports_controller.rb
+++ app/controllers/hours_reports_controller.rb
@@ -2,15 +2,15 @@ class HoursReportsController < ApplicationController
   before_filter :require_admin!
 
   def index
-		default_date_range
-		build_report
-	end
+    default_date_range
+    build_report
+  end
 
-	def create
-		param_date_range
-		build_report
-		render :action => :index
-	end
+  def create
+    param_date_range
+    build_report
+    render :action => :index
+  end
 
   private
   def hours_for(user, start_date, end_date = @end_date.beginning_of_week - 1.second)
@@ -24,21 +24,21 @@ class HoursReportsController < ApplicationController
         arr << time.strftime('%b %d %y')
       end
     end
-	end
+  end
 
-	def default_date_range
-		@start_date = Date.strptime((DateTime.now - 5.weeks).strftime('%m/%d/%Y'), '%m/%d/%Y')
-		@end_date = Date.strptime((DateTime.now).strftime('%m/%d/%Y'), '%m/%d/%Y')
-	end
+  def default_date_range
+    @start_date = Date.strptime((DateTime.now - 5.weeks).strftime('%m/%d/%Y'), '%m/%d/%Y')
+    @end_date = Date.strptime((DateTime.now).strftime('%m/%d/%Y'), '%m/%d/%Y')
+  end
 
-	def build_report
-		@users = User.all.select{ |u| hours_for(u, @start_date.beginning_of_week - 1.second).sum(:hours).to_s.to_f > 0.0 }
-		@sundays = week_endings(@start_date)
-	end
+  def build_report
+    @users = User.all.select{ |u| hours_for(u, @start_date.beginning_of_week - 1.second).sum(:hours).to_s.to_f > 0.0 }
+    @sundays = week_endings(@start_date)
+  end
 
-	def param_date_range
-		@start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
-		@end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
-	end
+  def param_date_range
+    @start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
+    @end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
+  end
 
 end
--- app/queries/work_unit_query.rb
+++ app/queries/work_unit_query.rb
@@ -1,18 +1,18 @@
 class WorkUnitQuery
 
-	def initialize(user, start_date, num_weeks_ago, scope)
-		@user = user
-		@num_weeks_ago = num_weeks_ago
-		@scope = scope
-		@start_date = start_date
-	end
+  def initialize(user, start_date, num_weeks_ago, scope)
+    @user = user
+    @num_weeks_ago = num_weeks_ago
+    @scope = scope
+    @start_date = start_date
+  end
 
-	def hours
-		if @scope == 'total'
-			@user.work_units.where(:start_time => @start_date.beginning_of_week - 1.second - ((@num_weeks_ago * 7) - 1).days..@start_date.beginning_of_week - 1.second - ((@num_weeks_ago - 1) * 7).days).sum(:hours).to_s.to_f
-		elsif @scope == 'billable' || @scope == 'unbillable'
-			@user.work_units.where(:start_time => @start_date.beginning_of_week - 1.second - ((@num_weeks_ago * 7) - 1).days..@start_date.beginning_of_week - 1.second - ((@num_weeks_ago - 1) * 7).days).send(@scope.to_sym).sum(:hours).to_s.to_f
-		end
-	end
+  def hours
+    if @scope == 'total'
+      @user.work_units.where(:start_time => @start_date.beginning_of_week - 1.second - ((@num_weeks_ago * 7) - 1).days..@start_date.beginning_of_week - 1.second - ((@num_weeks_ago - 1) * 7).days).sum(:hours).to_s.to_f
+    elsif @scope == 'billable' || @scope == 'unbillable'
+      @user.work_units.where(:start_time => @start_date.beginning_of_week - 1.second - ((@num_weeks_ago * 7) - 1).days..@start_date.beginning_of_week - 1.second - ((@num_weeks_ago - 1) * 7).days).send(@scope.to_sym).sum(:hours).to_s.to_f
+    end
+  end
 
 end
