22a6ccf241beeb4c305235c02da51deff70bc1b1
brookekfox <brookekfox@gmail.com>
2015-04-30 18:18:11 -0700

    removed hours method from users model
    [#93493736]

--- app/models/user.rb
+++ app/models/user.rb
@@ -143,12 +143,4 @@ class User < ActiveRecord::Base
       where(conditions).first
     end
   end
-
-	def hours(num_weeks_ago, scope)
-		if scope == 'total'
-			work_units.where(:start_time => Time.now.beginning_of_week - 1.second - ((num_weeks_ago * 7) - 1).days..Time.now.beginning_of_week - 1.second - ((num_weeks_ago - 1) * 7).days).sum(:hours).to_s.to_f
-		elsif scope == 'billable' || scope == 'unbillable'
-			work_units.where(:start_time => Time.now.beginning_of_week - 1.second - ((num_weeks_ago * 7) - 1).days..Time.now.beginning_of_week - 1.second - ((num_weeks_ago - 1) * 7).days).send(scope.to_sym).sum(:hours).to_s.to_f
-		end
-	end
 end
