a2a13c30d734459ae7a005de1b7b01bdb0e5d579
brookekfox <brookekfox@gmail.com>
2015-05-12 11:25:41 -0700

    changed the number of weeks between helper
    [#93493736]

--- app/queries/work_unit_query.rb
+++ app/queries/work_unit_query.rb
@@ -1,17 +1,17 @@
 class WorkUnitQuery
 
-  def initialize(user, start_date, num_weeks_ago, scope)
+  def initialize(user, end_of_week, num_weeks_ago, scope)
     @user = user
-		@start_date = start_date.beginning_of_week - 1.second
+		@end_of_week = end_of_week.beginning_of_week - 1.second
     @num_weeks_ago = num_weeks_ago
     @scope = scope
   end
 
   def hours
     if @scope == 'total'
-      @user.work_units.where(:start_time => @start_date - ((@num_weeks_ago * 7)).days..@start_date - ((@num_weeks_ago - 1) * 7).days).sum(:hours).to_s.to_f
+      @user.work_units.where(:start_time => @end_of_week - ((@num_weeks_ago * 7)).days..@end_of_week - ((@num_weeks_ago - 1) * 7).days).sum(:hours).to_s.to_f
     elsif @scope == 'billable' || @scope == 'unbillable'
-      @user.work_units.where(:start_time => @start_date - ((@num_weeks_ago * 7)).days..@start_date - ((@num_weeks_ago - 1) * 7).days).send(@scope.to_sym).sum(:hours).to_s.to_f
+      @user.work_units.where(:start_time => @end_of_week - ((@num_weeks_ago * 7)).days..@end_of_week - ((@num_weeks_ago - 1) * 7).days).send(@scope.to_sym).sum(:hours).to_s.to_f
     end
   end
 
