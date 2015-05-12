c90c0e3fb688077e65172a9e293bf993bc28fa5e
brookekfox <brookekfox@gmail.com>
2015-05-12 11:07:50 -0700

    added tabbed view for table and graph
    [#94354764]

--- app/helpers/hours_reports_helper.rb
+++ app/helpers/hours_reports_helper.rb
@@ -1,7 +1,14 @@
 module HoursReportsHelper
 
   def number_of_weeks_between(start_date, end_date)
-    (((end_date.beginning_of_week - start_date.beginning_of_week).to_i) / 7) + 1
+    (((end_date.beginning_of_week - start_date.beginning_of_week).to_i) / 7)
+  end
+
+  def hours_reports_data(users)
+    data = []
+    users.each do |u|
+      data << []
+    end
   end
 
 end
--- app/queries/work_unit_query.rb
+++ app/queries/work_unit_query.rb
@@ -2,16 +2,21 @@ class WorkUnitQuery
 
   def initialize(user, start_date, num_weeks_ago, scope)
     @user = user
-		@start_date = start_date.beginning_of_week - 1.second
+		@start_date = start_date.end_of_week - 1.week
     @num_weeks_ago = num_weeks_ago
     @scope = scope
   end
 
   def hours
     if @scope == 'total'
-      @user.work_units.where(:start_time => @start_date - ((@num_weeks_ago * 7)).days..@start_date - ((@num_weeks_ago - 1) * 7).days).sum(:hours).to_s.to_f
+      @user.work_units.where(:start_time => @start_date - (@num_weeks_ago * 7).days..@start_date - ((@num_weeks_ago - 1) * 7).days)
+                      .sum(:hours)
+                      .to_s.to_f
     elsif @scope == 'billable' || @scope == 'unbillable'
-      @user.work_units.where(:start_time => @start_date - ((@num_weeks_ago * 7)).days..@start_date - ((@num_weeks_ago - 1) * 7).days).send(@scope.to_sym).sum(:hours).to_s.to_f
+      @user.work_units.where(:start_time => @start_date - (@num_weeks_ago * 7).days..@start_date - ((@num_weeks_ago - 1) * 7).days)
+                      .send(@scope.to_sym)
+                      .sum(:hours)
+                      .to_s.to_f
     end
   end
 
