adf94ec283140d8ae24ae07aa828c5c37faeadac
brookekfox <brookekfox@gmail.com>
2015-05-15 16:18:20 -0700

    added controller method to remove queries from view
    [#93493736]

--- app/controllers/hours_reports_controller.rb
+++ app/controllers/hours_reports_controller.rb
@@ -1,15 +1,29 @@
 class HoursReportsController < ApplicationController
+  include HoursReportsHelper
   before_filter :require_admin!
 
   def index
     build_date_range
     build_report
+    @users_with_hours = users_with_hours(@users)
     render :action => :index
   end
 
   alias :create :index
 
   private
+  def users_with_hours(users)
+    users_with_hours = {}
+    users.each do |u|
+      users_with_hours[u.login.to_sym] = {
+        :billable => check_for_missing_weeks(WorkUnitQuery.new(u,@sundays[0],@sundays[@sundays.length-1],'billable').hours),
+        :unbillable => check_for_missing_weeks(WorkUnitQuery.new(u,@sundays[0],@sundays[@sundays.length-1],'unbillable').hours),
+        :total => check_for_missing_weeks(WorkUnitQuery.new(u,@sundays[0],@sundays[@sundays.length-1],'total').hours)
+      }
+    end
+    return users_with_hours
+  end
+
   def hours_for(user, start_date, end_date = @end_date.end_of_week - 1.week)
     user.work_units.where(:start_time => start_date..end_date).sum(:hours).to_s.to_f
   end
