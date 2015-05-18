4ecea75fe669502efabc68d887ab1da8ce667f9c
brookekfox <brookekfox@gmail.com>
2015-05-18 11:13:40 -0700

    added graph to view
    [#93493736]

--- app/helpers/hours_reports_helper.rb
+++ app/helpers/hours_reports_helper.rb
@@ -4,24 +4,25 @@ module HoursReportsHelper
     (((end_date.beginning_of_week - start_date.beginning_of_week).to_i) / 7)
   end
 
-  def check_for_missing_weeks(query)
+  def check_for_missing_weeks(query,sundays)
     query_sundays = []
     (0..query.length-1).each do |i|
       query_sundays << query[i][:sunday]
     end
-    missing_sundays = @sundays - query_sundays
+    missing_sundays = sundays - query_sundays
     (0..missing_sundays.length-1).each do |j|
       query.push( {:sunday => missing_sundays[j], :hours => 0.0} )
     end
     return query.sort_by{ |hash| Date.parse(hash[:sunday]) }
   end
 
-  def hours_reports_data(users,sundays,scope)
+  def hours_reports_data(users,sundays,kind)
     dataset = []
     users.each do |u|
+      query = check_for_missing_weeks(WorkUnitQuery.new(u,sundays[0],sundays[sundays.length-1],kind).hours, sundays)
       points = []
-      sundays.each do |sun|
-        points << [(Date.parse(sun)).strftime('%s').to_i, WorkUnitQuery.new(u,sun,sun,scope).hours]
+      (0..query.length-1).each do |i|
+        points << [Date.parse(query[i][:sunday]).strftime('%s').to_i, query[i][:hours]]
       end
       dataset << points
     end
