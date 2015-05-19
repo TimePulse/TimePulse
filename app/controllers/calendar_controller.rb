4e18d7ea997d1c3a989d226c2a1fd0d880b6358a
Rick Gibson <ricklesgibson@gmail.com>
2015-05-19 13:43:35 -0700

    Converted to ERB

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -8,6 +8,14 @@ class CalendarController < ApplicationController
     end
     @colorsArray = ['#0099FF', '#66FF66', '#CCFF33', '#FF6600', '#FF3399', '#CC66FF', '#666699', '#800000', '#339933', '#FFFF00', '#003366', '#CC9900', '#3399FF', '#00FFFF', '#660033', '#003366', '#CC00CC', '#000066']
 
+    @calendarArgs = @users.each_with_index.map do |user, index|
+                "{
+                  url: '/calendar_work_units.json?user_id=#{user.id}',
+                  color: '#{@colorsArray[index % @colorsArray.length]}',
+                  textColor: 'black'
+                }"
+            end.join(', ')
+
     respond_to do |format|
       format.html
     end
