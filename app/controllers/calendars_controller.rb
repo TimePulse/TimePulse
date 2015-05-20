a750b37d84fb8fa81a2167f4c4dcf42b422275ef
Rick Gibson <ricklesgibson@gmail.com>
2015-05-20 14:50:46 -0700

    Users sorted by name, displayed with colors by index
    [#94878270]

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
    before_filter :require_user!
   def index
     if current_user.admin?
-      @users = User.active
+      @users = User.active.order("name ASC")
     else
       @users = [current_user]
     end
