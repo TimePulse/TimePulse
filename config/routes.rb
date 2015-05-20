9eb5a309ff89e41fdea820879ed9a9a49a46e42d
Evan Dorn <evan@lrdesign.com>
2015-05-20 12:24:14 -0700

    Revert "Add calendar view for work units"
    This reverts commit e6d0f4cd568f57e1397e9cf45b3e33fb6cc398ab.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ TimePulse::Application.routes.draw do
   resources :permissions
   resources :work_units, :except => :index
   resources :clients
-  resources :calendar_work_units, :only => :index
   resources :projects do
     resource :github_pull, :controller => 'github_pull', :only => [:create]
     resource :pivotal_pull, :controller => 'pivotal_pull', :only => [:create]
