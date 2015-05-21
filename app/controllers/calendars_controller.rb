9ce0c6c616a4d90d79f8a20b95da5c6dc661baa0
Rick Gibson <ricklesgibson@gmail.com>
2015-05-21 13:02:46 -0700

    Added in a calendar_controller_spec
    [#94878270]

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,14 +7,6 @@ class CalendarsController < ApplicationController
       @users = [current_user]
     end
 
-    @calendarArgs = @users.each_with_index.map do |user, index|
-                "{
-                  url: '/calendar_work_units.json?user_id=#{user.id}',
-                  color: '#{COLORS_ARRAY[index % COLORS_ARRAY.length]}',
-                  textColor: 'black'
-                }"
-            end.join(', ')
-
     respond_to do |format|
       format.html
     end
