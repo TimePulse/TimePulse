da0af97e08f0684f5fbcc9f2b5fc35253c5d3221
Rick Gibson <ricklesgibson@gmail.com>
2015-05-21 12:47:26 -0700

    Fix spec fails and set CalendarWorkUnitsController to inherit from ApplicationController
    [#94878270]

--- app/controllers/calendar_work_units_controller.rb
+++ app/controllers/calendar_work_units_controller.rb
@@ -1,16 +1,15 @@
-class CalendarWorkUnitsController < CalendarsController
+class CalendarWorkUnitsController < ApplicationController
 
   before_filter :require_user!
   def index
     user = User.find(params[:user_id])
     @start_time = params[:start]
     @end_time = params[:end]
-      # if @start_time && @end_time
-      #   @work_units = current_user.work_units.where('start_time >= ?', Time.parse(@start_time).utc).where('stop_time <= ?', Time.parse(@end_time).utc)
-      # else
-    @work_units = user.work_units.where('start_time >= ?', Time.parse(@start_time).utc).where('stop_time <= ?', Time.parse(@end_time).utc)
-
-
+    if @start_time && @end_time
+      @work_units = user.work_units.where('start_time >= ?', Time.parse(@start_time).utc).where('stop_time <= ?', Time.parse(@end_time).utc)
+    else
+      @work_units = user.work_units
+    end
     respond_to do |format|
     format.json { render :json => @work_units, each_serializer: CalendarEventSerializer, root: false}
     end
