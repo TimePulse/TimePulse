c07f31abb889da328df3105c5767acd4f0b56976
Rick Gibson <ricklesgibson@gmail.com>
2015-05-26 20:35:28 -0700

    Spec is now hitting the endpoint but activity is not saving
    [#95179290]

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -19,12 +19,12 @@
     end
 
     describe "hit endpoint with a post request that saves activity" do
-      it "should responsd with a 201 status" do
+      it "should respond with a 201 status" do
         request.headers["accept"] = 'application/json'
         request.headers["Content-Type"] = 'application/json'
         request.headers["login"] = @user.login
         request.headers["Authorization"] = 'AEsXr_Ec6R_trmAoLd5S'
-        p post :create, :params['activity'] = {:activity => @activity.to_json}
+        p post :create, {activity: {description: "UPDATE AUTH TEST EXPICCSAASD", project_id: 4, source: "QWE"}}
         expect(response.status).to eq(201)
       end
     end
