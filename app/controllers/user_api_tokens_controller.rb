0249f30eb9fdc96b6fec2770679ac8e8cf9d975f
Rick Gibson <ricklesgibson@gmail.com>
2015-06-01 16:05:26 -0700

    Change from Devise.friendly_token method to SecureRandom for token generation. Change made to regex in spec
    [#95179290]

--- app/controllers/user_api_tokens_controller.rb
+++ app/controllers/user_api_tokens_controller.rb
@@ -3,7 +3,7 @@ class UserApiTokensController < ApplicationController
   before_filter :require_user!
   def update
     @user = current_user
-    @unencrypted_token = Devise.friendly_token
+    @unencrypted_token = SecureRandom.urlsafe_base64(15)
     @encrypted_token = BCrypt::Password.create(@unencrypted_token)
     @user.encrypted_token = @encrypted_token
     @user.save
