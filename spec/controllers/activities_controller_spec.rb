19335fd184f3fccc9a45b07fbb453d9158717c6c
Rick Gibson <ricklesgibson@gmail.com>
2015-06-02 14:32:17 -0700

    Initial setup and testing
    [#95894180]

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -3,9 +3,12 @@
   describe ActivitiesController do
 
     before(:each) do
-      @activity = FactoryGirl.create(:activity)
+      @base_time = Time.now.beginning_of_day-1.day+12.hours
       @user = FactoryGirl.create(:user)
       @project = FactoryGirl.create(:project)
+      #create activity and work_unit that both have the same project associated
+      @activity = FactoryGirl.create(:activity, :project => @project)
+      @current_work_unit = FactoryGirl.create(:work_unit, :project => @project, :start_time => @base_time, :stop_time => nil, :user => @user, :hours => nil, :notes => "Work Unit, Clocked in")
     end
 
     describe "hit endpoint with a post request that saves activity" do
@@ -29,5 +32,15 @@
             Activity.where(description: "UPDATE AUTH TEST").count
             }.from(0).to(1)
       end
+    end
+
+    describe "make a new activity that is associated with the current work unit, if clocked_in" do
+      it "checks to see if the user is clocked in" do
+        expect(@user.clocked_in?).to be true
+      end
+      it "confirms current_work_unit & activity request have the same project associated" do
+        expect(@current_work_unit.project_id).to eq(@activity.project_id)
+      end
+
     end
   end
