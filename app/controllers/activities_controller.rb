84a24cd69b42cd334ba186070844a5ec69173631
Rick Gibson <ricklesgibson@gmail.com>
2015-06-02 18:34:20 -0700

    Logic for users controller to check project id
    [#95894180]

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,23 @@
 require 'bcrypt'
 class ActivitiesController < ApplicationController
+  include WorkUnitsHelper
   before_action :restrict_access, only: [:create]
 
   def create
+    @user = User.where(:login => request.headers["login"]).first
+    @current_work_unit = @user.work_units.where(:stop_time => nil).last
     @activity = Activity.new(activity_params)
+    p @current_work_unit.id
+    #check to see if the user making the request has an open work unit
+    if @current_work_unit
+      p "there is an open work unit"
+      if @current_work_unit.project_id == @activity.project_id
+        p "project_id is equal"
+        @activity.work_unit_id = @current_work_unit.id
+        p @activity.work_unit_id
+      end
+    end
+
       if @activity.save
         render json: @activity, status: 201
       else
@@ -14,16 +28,16 @@ class ActivitiesController < ApplicationController
 private
 
   def activity_params
-    params.require(:activity).permit(:description, :project_id, :source, :time, :action, :user_id, properties: [:story_id])
+    params.require(:activity).permit(:description, :work_unit_id, :project_id, :source, :time, :action, :user_id, properties: [:story_id])
   end
 
   def restrict_access
-    user = User.where(:login => request.headers["login"]).first
-    unless user = User.where(:login => request.headers["login"]).first
+    @user = User.where(:login => request.headers["login"]).first
+    unless @user = User.where(:login => request.headers["login"]).first
       render json: "authorization failed no user" , status: 403
     end
     utoken = request.headers["Authorization"]
-    if BCrypt::Password.new(user.encrypted_token) == utoken
+    if BCrypt::Password.new(@user.encrypted_token) == utoken
       return true
     else
       render json: "authorization failed wrong token", status: 403
--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -6,9 +6,13 @@
       @base_time = Time.now.beginning_of_day-1.day+12.hours
       @user = FactoryGirl.create(:user)
       @project = FactoryGirl.create(:project)
+      @project_different = FactoryGirl.create(:project)
       #create activity and work_unit that both have the same project associated
-      @activity = FactoryGirl.create(:activity, :project => @project)
+      @activity = FactoryGirl.create(:activity, :project => @project, :user => @user)
       @current_work_unit = FactoryGirl.create(:work_unit, :project => @project, :start_time => @base_time, :stop_time => nil, :user => @user, :hours => nil, :notes => "Work Unit, Clocked in")
+      #create activity and work_unit with different projects associated
+      @activity_different = FactoryGirl.create(:activity, :project => @project_different, :user => @user)
+      @activity_request = {activity: {description: @activity.description, project_id: @activity.project_id, source: @activity.source, time: @base_time, user_id: @user.id}}
     end
 
     describe "hit endpoint with a post request that saves activity" do
@@ -17,7 +21,7 @@
         request.headers["Content-Type"] = 'application/json'
         request.headers["login"] = @user.login
         request.headers["Authorization"] = 'AEsXr_Ec6R_trmAoLd5S'
-        post :create, {activity: {description: "UPDATE AUTH TEST", project_id: 1  , source: "QWE"}}
+        post :create, @activity_request
         expect(response.status).to eq(201)
       end
 
@@ -27,10 +31,10 @@
           request.headers["Content-Type"] = 'application/json'
           request.headers["login"] = @user.login
           request.headers["Authorization"] = 'AEsXr_Ec6R_trmAoLd5S'
-          post :create, {activity: {description: "UPDATE AUTH TEST", project_id: 1  , source: "QWE"}}
+          post :create, @activity_request
         end.to change{
-            Activity.where(description: "UPDATE AUTH TEST").count
-            }.from(0).to(1)
+            Activity.where(description: @activity.description).count
+            }.from(2).to(3)
       end
     end
 
@@ -40,6 +44,9 @@
       end
       it "confirms current_work_unit & activity request have the same project associated" do
         expect(@current_work_unit.project_id).to eq(@activity.project_id)
+      end
+      it "confirms current_work_unit and activity_different don't have to same project associated"do
+        expect(@current_work_unit.project_id).not_to eq(@activity_different.project_id)
       end
 
     end
