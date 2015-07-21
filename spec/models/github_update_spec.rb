657543fef24defddbad45008b665795482cd1e95
Tablesaw Tablesawsen <tablesaw@gmail.com>
2015-07-21 20:24:17 +0000

    Reformat JSON payload for github_update spec
    Reformatted and modified the JSON payload for github_update spec.
    The dummy information is now in line with the information from
    github pull. A second commit has been added to the push event to
    test uniqueness.

--- spec/models/github_update_spec.rb
+++ spec/models/github_update_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe GithubUpdate do
+  
+  # See https://developer.github.com/v3/activity/events/types/#pushevent
+  # for formatting of a github push webhook.
   let :payload_file do
     File.join(Rails.root, '/spec/support/fixtures/sample_github_payload.txt')
   end
@@ -18,13 +21,13 @@ describe GithubUpdate do
   end
   
   let! :repo do
-    FactoryGirl.create(:repository, :project => project,
-      :url => "https://github.com/hannahhoward/activerecord-postgis-array")
+    FactoryGirl.create(:repository, project: project,
+      url: "https://github.com/Correct-User/Repo-One")
   end 
 
 
   let! :user do
-    FactoryGirl.create(:user, :github_user => "hannahhoward")
+    FactoryGirl.create(:user, :github_user => "one")
   end
 
   let :start_time do DateTime.parse("2013-05-23T16:48:39-07:00").advance(:minutes => -15) end
@@ -33,11 +36,11 @@ describe GithubUpdate do
   let :last_activity do Activity.last end
 
   describe "save" do
-    it "should create an activity" do
+    it "should create two activities" do
       expect do
         github_update = GithubUpdate.new(params)
         github_update.save
-      end.to change{Activity.count}.by(1)
+      end.to change{Activity.count}.by(2)
     end
 
     it "should set the parameters properly" do
@@ -45,12 +48,13 @@ describe GithubUpdate do
       github_update.save
       last_activity.source.should == "github"
       last_activity.action.should == "commit"
-      last_activity.description.should == "Update Version"
+      last_activity.description.should == "Message 2"
       last_activity.time.should == "2013-05-23T16:48:39-07:00"
-      last_activity.properties['id'].should == "042ec79f159fe8f0b7a520330f05dfda8741cc75"
+      last_activity.properties['id'].should == "sha2"
       last_activity.properties['branch'].should == "master"
       last_activity.project.should == project
       last_activity.user.should == user
     end
   end
 end
+
