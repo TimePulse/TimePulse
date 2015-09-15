2b54afc480b3bdd3d626595d991d5a33f51fee6b
Anne Vetto <vettoap@gmail.com>
2015-09-15 11:25:29 -0700

    Change devise.rb to use secret_key_base.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = Rails.application.secrets.email['from']
-  config.secret_key = Rails.application.secrets.devise_secret_key
+  config.secret_key = Rails.application.secrets.secret_key_base
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
 
