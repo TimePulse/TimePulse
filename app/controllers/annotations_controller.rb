e1c9bb806cb2c5c48f0ffadf51b98d941c89a111
Anne Vetto <vettoap@gmail.com>
2015-10-12 17:37:48 -0700

    Make annotations dynamically appear under timeclock after submit form.

---
+++ app/controllers/annotations_controller.rb
@@ -0,0 +1,30 @@
+class AnnotationsController < ApplicationController
+  before_filter :require_user!
+
+  #POST
+  def create
+
+    @current_work_unit = current_user.current_work_unit
+    @activity = Activity.new(activity_params)
+    #check to see if the user making the request has an open work unit
+    #
+    if @current_work_unit.present?
+      if @current_work_unit.project == @activity.project
+        @activity.work_unit = @current_work_unit
+      end
+    end
+
+    respond_to do |format|
+      if @activity.save
+        flash[:notice] = 'Annotation was successfully created.'
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activities).permit(:description, :work_unit_id, :project_id, :source, :time, :action, :user_id, properties: [:story_id])
+  end
+end
