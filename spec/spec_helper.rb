f3d1232f9d235ca93698d0f96da8d76e965e8530
Judson <nyarly@gmail.com>
2015-12-02 16:37:44 -0800

    Simple fix to a spec fail, and hopefully a finish to the data timeouts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,11 +53,7 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :helper
 
   config.before(:suite) do
-    app = Rails.application
-    index = Sprockets::Environment.new.index
-    output = File.join(app.root, 'public', app.config.assets.prefix)
-    assets = app.config.assets.precompile
-    Sprockets::Manifest.new(index, output).compile(assets)
+    %x{rake assets:precompile}
   end
 
   config.after(:each, :type => :view)  do
